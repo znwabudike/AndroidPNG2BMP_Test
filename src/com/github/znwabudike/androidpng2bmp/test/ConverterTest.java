5343c7f41ce6a728a19fe37c5a553784afb149e6
znwabudike <zachary.nwabudike@gmail.com>
2014-10-30 23:29:15 -0700

    fixing stuff

--- src/com/github/znwabudike/androidpng2bmp/test/ConverterTest.java
+++ src/com/github/znwabudike/androidpng2bmp/test/ConverterTest.java
@@ -1,16 +1,12 @@
 package com.github.znwabudike.androidpng2bmp.test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+
 import java.io.IOException;
-import java.io.InputStream;
+
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.test.ActivityInstrumentationTestCase;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.ActivityTestCase;
-import android.util.Log;
+import com.github.znwabudike.alogger.Log;
 
 import com.github.znwabudike.androidpng2bmp.Converter;
 import com.github.znwabudike.androidpng2bmp.hackactivity.Hacktivity;
@@ -20,8 +16,9 @@ public class ConverterTest extends ActivityInstrumentationTestCase2<Hacktivity>{
 
 	private Converter converter;
 	private Hacktivity hacktivity;
-	public byte[] byteArray;
-	private boolean isPNG;
+	public byte[] bmpByteArray;
+	public byte[] pngByteArray;
+	private boolean isPNG = true;
 	public ConverterTest(){
 		super(Hacktivity.class);
 	}
@@ -40,24 +37,35 @@ public class ConverterTest extends ActivityInstrumentationTestCase2<Hacktivity>{
 	}
 
 	public void testIsBMHeader(){
-		assertTrue(converter.isBM(byteArray));
+		assertTrue(converter.isBM(bmpByteArray));
 	}
 	public void testIsPNGHeader(){ 
-		assertTrue(isPNG);
+		assertTrue(converter.isPNGHeader(pngByteArray));
 	}
 	
 	public void testGetArray() throws IOException {
 		Context testContext = getInstrumentation().getContext();
-		Resources testRes = testContext.getResources();
-		InputStream streamIn = (InputStream) testRes.openRawResource(R.raw.copy);
-		byteArray = new byte[streamIn.available()];
-		for(int i = 0; i < byteArray.length; i++){
-			byteArray[i] = (byte) streamIn.read();
-		}
-		log("length = " + byteArray.length);
-		assertNotNull(byteArray);
+		bmpByteArray = converter.getArrayFromResource(testContext, R.raw.bmp);
+		log("bmp bytes = " + bytesToHex(bmpByteArray));
+		assertNotNull(bmpByteArray);
+		
+		pngByteArray = converter.getArrayFromResource(testContext, R.raw.copy);
+		log("png bytes = " + bytesToHex(pngByteArray));
+		assertNotNull(pngByteArray);
+		
 	}
 
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+	
 	private void log(String msg) {
 		String TAG = ConverterTest.class.getSimpleName();
 		if (BuildConfig.DEBUG) {
